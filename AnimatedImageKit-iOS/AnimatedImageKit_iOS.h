108e02497516840b4a50ed83803c55943218eed8
Zhu Shengqi <zetasq@gmail.com>
2017-11-12 16:50:34 +0800

    Project setup

---
+++ AnimatedImageKit-iOS/AnimatedImageKit_iOS.h
@@ -0,0 +1,18 @@
+//
+//  AnimatedImageKit_iOS.h
+//  AnimatedImageKit-iOS
+//
+//  Created by Zhu Shengqi on 12/11/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnimatedImageKit_iOS.
+FOUNDATION_EXPORT double AnimatedImageKit_iOSVersionNumber;
+
+//! Project version string for AnimatedImageKit_iOS.
+FOUNDATION_EXPORT const unsigned char AnimatedImageKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimatedImageKit_iOS/PublicHeader.h>
+
+
---
+++ Sources/AnimatedImage.swift
@@ -0,0 +1,12 @@
+//
+//  AnimatedImage.swift
+//  AnimatedImageKit-iOS
+//
+//  Created by Zhu Shengqi on 12/11/2017.
+//
+
+import Foundation
+
+public class AnimatedImage {
+    
+}
